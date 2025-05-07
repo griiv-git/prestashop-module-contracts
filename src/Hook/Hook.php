555e74eecdbce7767082bc5d452160422a5a39f7
Arnaud <arnaud.scote@gmail.com>
2025-05-07 09:26:36 +0200

    Hook class dont need module instance in constructor + add exception

--- src/Hook/Hook.php
+++ src/Hook/Hook.php
@@ -8,18 +8,50 @@ use DgtxContactForm;
 class Hook
 {
     protected $context;
-    protected $module;
+    protected $module = null;
     protected $tpl = null;
     private $tplExt = '.tpl';
     protected $tplName = null;
 
-    public function __construct(\Module $module, \Context $context)
+    public function __construct(\Context $context)
     {
         $this->context = $context;
+    }
+
+    protected function getModule()
+    {
+        return $this->module;
+    }
+
+    protected function setModule(\Module $module)
+    {
         $this->module = $module;
+    }
+
+    protected function getTpl()
+    {
+        if (!$this->getModule() instanceof \Module) {
+            throw new \Exception('Module not set');
+        }
 
         if (null !== $this->tplName) {
             $this->tpl = $this->module->tplPath.$this->tplName.$this->tplExt;
         }
+
+        if ($this->tpl === null) {
+            throw new \Exception('Template not set, please set it with setTplName() method');;
+        }
+
+        return $this->tpl;
+    }
+
+    protected function setTplName($tplName)
+    {
+        $this->tplName = $tplName;
+    }
+
+    protected function getTplName()
+    {
+        return $this->tplName;
     }
 }
